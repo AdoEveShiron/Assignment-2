12fb35687a06b3dcca25c619705ab47c9c4a14bc
AdoEveShiron <sonata_funds2f@icloud.com>
2026-04-12 23:37:11 +0500

    Update MyArrayList.java
    implement MyArrayList

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,2 +1,163 @@
-public class MyArrayList {
+import java.util.Iterator;
+
+public class MyArrayList<T> implements MyList<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity <= elements.length) {
+            return;
+        }
+
+        int newCapacity = elements.length * 2;
+        if (newCapacity < minCapacity) {
+            newCapacity = minCapacity;
+        }
+
+        Object[] newArray = new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = elements[i];
+        }
+        elements = newArray;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", size: " + size);
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", size: " + size);
+        }
+    }
+
+    @Override
+    public void add(T element) {
+        ensureCapacity(size + 1);
+        elements[size] = element;
+        size++;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        checkIndexForAdd(index);
+        ensureCapacity(size + 1);
+
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+
+        elements[index] = element;
+        size++;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        checkIndex(index);
+        return (T) elements[index];
+    }
+
+    @Override
+    public T set(int index, T element) {
+        checkIndex(index);
+        T oldValue = get(index);
+        elements[index] = element;
+        return oldValue;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T remove(int index) {
+        checkIndex(index);
+
+        T removed = (T) elements[index];
+
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+
+        elements[size - 1] = null;
+        size--;
+        return removed;
+    }
+
+    @Override
+    public boolean remove(T element) {
+        int index = indexOf(element);
+        if (index == -1) {
+            return false;
+        }
+        remove(index);
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public boolean contains(T element) {
+        return indexOf(element) != -1;
+    }
+
+    @Override
+    public int indexOf(T element) {
+        for (int i = 0; i < size; i++) {
+            if (element == null) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            } else {
+                if (element.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public T next() {
+                if (!hasNext()) {
+                    throw new IndexOutOfBoundsException("No more elements");
+                }
+                return (T) elements[cursor++];
+            }
+        };
+    }
 }
